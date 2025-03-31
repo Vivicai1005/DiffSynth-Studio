26e856eff9d14980370b15a66dc0e4b642c8ca09
Vivicai1005 <caiweiwei1005@gmail.com>
2025-03-31 17:23:35 +0800

    edit docker image

--- examples/stepvideo/step2video_docker.sh
+++ examples/stepvideo/step2video_docker.sh
@@ -3,4 +3,4 @@
 sudo docker run -it --name step2video --ipc=host --cap-add=SYS_PTRACE --network=host \
  --device=/dev/kfd --device=/dev/dri --security-opt seccomp=unconfined \
  -v /home/weicai:/work --group-add video --privileged -w /work \
- rocm/pytorch:rocm6.3.3_ubuntu22.04_py3.9_pytorch_release_2.4.0
+ rocm/pytorch:latest
