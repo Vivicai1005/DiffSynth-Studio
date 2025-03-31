3d6f11d2048b1dcd6c61a1f54f1679f6e6c5bb43
Vivicai1005 <caiweiwei1005@gmail.com>
2025-03-31 15:56:46 +0800

    add docker file

---
+++ examples/stepvideo/step2video_docker.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# This script runs the Docker container in detached mode
+docker run -d --name step2video --ipc=host --cap-add=SYS_PTRACE --network=host \
+ --device=/dev/kfd --device=/dev/dri --security-opt seccomp=unconfined \
+ -v /home/weicai:/work --group-add video --privileged -w /work\
+ rocm/pytorch:rocm6.3.3_ubuntu22.04_py3.9_pytorch_release_2.4.0
