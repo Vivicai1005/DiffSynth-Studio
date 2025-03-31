4d6e07e2fd04bfde0e9ae5bcdcfbacc2c6ead72d
Vivicai1005 <caiweiwei1005@gmail.com>
2025-03-31 16:05:19 +0800

    edit command for starting docker

--- examples/stepvideo/step2video_docker.sh
+++ examples/stepvideo/step2video_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # This script runs the Docker container in detached mode
-sudo docker run -d --name step2video --ipc=host --cap-add=SYS_PTRACE --network=host \
+sudo docker run -it --name step2video --ipc=host --cap-add=SYS_PTRACE --network=host \
  --device=/dev/kfd --device=/dev/dri --security-opt seccomp=unconfined \
- -v /home/weicai:/work --group-add video --privileged -w /work\
+ -v /home/weicai:/work --group-add video --privileged -w /work \
  rocm/pytorch:rocm6.3.3_ubuntu22.04_py3.9_pytorch_release_2.4.0
