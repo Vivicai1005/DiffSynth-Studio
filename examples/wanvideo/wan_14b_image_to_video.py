5b8c6b304296895fe2b574e9af5552c7cd7cb233
Vivicai1005 <caiweiwei1005@gmail.com>
2025-04-09 14:58:38 +0800

    change input image

--- examples/wanvideo/wan_14b_image_to_video.py
+++ examples/wanvideo/wan_14b_image_to_video.py
@@ -38,11 +38,11 @@ pipe.enable_vram_management(num_persistent_param_in_dit=6*10**9) # You can set `
 #     local_dir="./",
 #     allow_file_pattern=f"data/examples/wan/input_image.jpg"
 # )
-image = Image.open("data/examples/wan/input_image.jpg")
+image = Image.open("data/examples/wan/grey_shoe.png")
 
 # Image-to-video
 video = pipe(
-    prompt="一艘小船正勇敢地乘风破浪前行。蔚蓝的大海波涛汹涌，白色的浪花拍打着船身，但小船毫不畏惧，坚定地驶向远方。阳光洒在水面上，闪烁着金色的光芒，为这壮丽的场景增添了一抹温暖。镜头拉近，可以看到船上的旗帜迎风飘扬，象征着不屈的精神与冒险的勇气。这段画面充满力量，激励人心，展现了面对挑战时的无畏与执着。",
+    prompt="A high-quality 3D product showcase video of a modern slip-on athletic knit shoe in dark gray with a white sole, spinning slowly on a white seamless background. The shoe has a breathable mesh texture, minimalist design, and a small side logo. Show different angles including top view, side view, sole view, and close-ups of the knit pattern and logo. Lighting is soft and professional, emphasizing the shoe’s fabric texture and contours.",
     negative_prompt="色调艳丽，过曝，静态，细节模糊不清，字幕，风格，作品，画作，画面，静止，整体发灰，最差质量，低质量，JPEG压缩残留，丑陋的，残缺的，多余的手指，画得不好的手部，画得不好的脸部，畸形的，毁容的，形态畸形的肢体，手指融合，静止不动的画面，杂乱的背景，三条腿，背景人很多，倒着走",
     input_image=image,
     num_inference_steps=50,
