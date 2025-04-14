c966e3f62f30c8a97c70a7d9263f083dd271d10b
Vivicai1005 <caiweiwei1005@gmail.com>
2025-04-14 17:33:18 +0800

    edit text prompt, height and width

--- examples/wanvideo/wan_14b_text_to_video_usp.py
+++ examples/wanvideo/wan_14b_text_to_video_usp.py
@@ -49,9 +49,11 @@ pipe.enable_vram_management(num_persistent_param_in_dit=None) # You can set `num
 
 # Text-to-video
 video = pipe(
-    prompt="一名宇航员身穿太空服，面朝镜头骑着一匹机械马在火星表面驰骋。红色的荒凉地表延伸至远方，点缀着巨大的陨石坑和奇特的岩石结构。机械马的步伐稳健，扬起微弱的尘埃，展现出未来科技与原始探索的完美结合。宇航员手持操控装置，目光坚定，仿佛正在开辟人类的新疆域。背景是深邃的宇宙和蔚蓝的地球，画面既科幻又充满希望，让人不禁畅想未来的星际生活。",
+    prompt="A professional and elegant East Asian woman in her late twenties to early thirties is hosting a talk show or giving a presentation in a luxurious, well-lit studio. She is seated at a round, black glass table with golden accents, with a sleek silver laptop open in front of her. She wears a pastel pink blazer over a mint green blouse, radiating warmth and confidence. The woman speaks with expressive hand gestures and a friendly smile, occasionally looking at the camera as if addressing the audience. The background features a stylish interior with soft lighting, wooden panels, bookshelves, and tasteful décor, creating a sophisticated atmosphere. Smooth camera movement captures her gestures and expressions, highlighting her professionalism and charm.",
     negative_prompt="色调艳丽，过曝，静态，细节模糊不清，字幕，风格，作品，画作，画面，静止，整体发灰，最差质量，低质量，JPEG压缩残留，丑陋的，残缺的，多余的手指，画得不好的手部，画得不好的脸部，畸形的，毁容的，形态畸形的肢体，手指融合，静止不动的画面，杂乱的背景，三条腿，背景人很多，倒着走",
     num_inference_steps=50,
+    height=1248,
+    width=720,
     num_frames=205,
     seed=0, tiled=True
 )
