a3cddf77169389054738fd2b615186b551ae381b
Vivicai1005 <caiweiwei1005@gmail.com>
2025-04-15 14:28:00 +0800

    edit text prompt

--- examples/wanvideo/wan_14b_text_to_video_usp.py
+++ examples/wanvideo/wan_14b_text_to_video_usp.py
@@ -49,13 +49,13 @@ pipe.enable_vram_management(num_persistent_param_in_dit=None) # You can set `num
 
 # Text-to-video
 video = pipe(
-    prompt="A professional and elegant East Asian woman in her late twenties to early thirties is hosting a talk show or giving a presentation in a luxurious, well-lit studio. She is seated at a round, black glass table with golden accents, with a sleek silver laptop open in front of her. She wears a pastel pink blazer over a mint green blouse, radiating warmth and confidence. The woman speaks with expressive hand gestures and a friendly smile, occasionally looking at the camera as if addressing the audience. The background features a stylish interior with soft lighting, wooden panels, bookshelves, and tasteful décor, creating a sophisticated atmosphere. Smooth camera movement captures her gestures and expressions, highlighting her professionalism and charm.",
+    prompt="A stunningly beautiful and charming East Asian woman in her late twenties to early thirties is hosting a talk show or delivering a presentation in a luxurious, well-lit studio. She has flawless, radiant skin, soft features, and expressive, sparkling eyes that convey warmth and intelligence. Her long, silky hair is elegantly styled, complementing her graceful appearance. Seated at a round, black glass table with golden accents, she has a sleek silver laptop open in front of her. She wears a pastel pink tailored blazer over a mint green silk blouse, perfectly blending professional elegance with a soft, approachable charm. Her friendly smile, gentle voice, and animated hand gestures make her presence both captivating and adorable. The background features a tastefully designed interior with warm ambient lighting, wooden panels, neatly arranged bookshelves, and minimalist décor, creating a cozy yet sophisticated atmosphere. Smooth camera movement captures her every expression and gesture, highlighting her beauty, charisma, and confident professionalism.",
     negative_prompt="色调艳丽，过曝，静态，细节模糊不清，字幕，风格，作品，画作，画面，静止，整体发灰，最差质量，低质量，JPEG压缩残留，丑陋的，残缺的，多余的手指，画得不好的手部，画得不好的脸部，畸形的，毁容的，形态畸形的肢体，手指融合，静止不动的画面，杂乱的背景，三条腿，背景人很多，倒着走",
     num_inference_steps=50,
-    height=1248,
+    height=1280,
     width=720,
-    num_frames=205,
+    num_frames=225,
     seed=0, tiled=True
 )
 if dist.get_rank() == 0:
-    save_video(video, "video1.mp4", fps=25, quality=5)
+    save_video(video, "video1.mp4", fps=25, quality=9)
