1d33217dcb637d85fc7e5efb0b5d0074daaa2107
Vivicai1005 <caiweiwei1005@gmail.com>
2025-04-28 11:08:05 +0800

    edit input image

--- examples/wanvideo/wan_14b_image_to_video_usp_2.py
+++ examples/wanvideo/wan_14b_image_to_video_usp_2.py
@@ -36,7 +36,7 @@ model_manager.load_models(
 #     local_dir="./",
 #     allow_file_pattern=f"data/examples/wan/input_image.jpg"
 # )
-image = Image.open("data/dr_su_2.png")
+image = Image.open("data/dr_su2_last_frame.png")
 
 dist.init_process_group(
     backend="nccl",
@@ -62,7 +62,7 @@ pipe.enable_vram_management(num_persistent_param_in_dit=None) # You can set `num
 
 # Image-to-video
 video = pipe(
-    prompt="A realistic, high-resolution video capturing a distinguished East Asian woman in her early fifties, standing confidently on a modern stage with a dark, textured backdrop. She has short, neatly styled gray hair, wears elegant glasses, and is dressed in a minimalist dark blue stand-collar jacket over a black blouse. A discreet headset microphone is positioned near her cheek, and she holds a sleek presentation remote in one hand. Her facial expression is focused and expressive as she clearly articulates a key point about the transformative role of education in technological advancement. She uses precise, minimal hand gestures — one hand subtly emphasizing her words while the other holds the remote. The stage lighting softly highlights her professional presence, while the steady medium shot frames her upper body, creating an atmosphere of authority and clarity, engaging the audience with her insightful message.",
+    prompt="The video continues seamlessly on the same stage with the distinguished East Asian woman standing tall against the minimalist dark backdrop. Her expression shifts from thoughtful engagement to one of firm determination and visionary leadership. Her intelligent eyes reflect resolve and a clear call to action as she speaks passionately about the urgent need for innovation and equity in education. She holds a presentation remote confidently in one hand, while using precise and assertive gestures with the other to drive her points home. The camera slowly pushes in to emphasize the intensity of her gaze and the conviction in her voice. The stage lighting subtly sharpens, highlighting her presence as a leader inspiring change. The overall mood conveys authority, clarity, and an inspiring commitment to shaping the future of education.",
     negative_prompt="色调艳丽，过曝，静态，细节模糊不清，字幕，风格，作品，画作，画面，静止，整体发灰，最差质量，低质量，JPEG压缩残留，丑陋的，残缺的，多余的手指，画得不好的手部，画得不好的脸部，畸形的，毁容的，形态畸形的肢体，手指融合，静止不动的画面，杂乱的背景，三条腿，背景人很多，倒着走",
     input_image=image,
     height=760,
@@ -71,4 +71,4 @@ video = pipe(
     seed=1, tiled=True
 )
 if dist.get_rank() == 0:
-    save_video(video, "wan14_i2v_720p_dr_su_1.mp4", fps=15, quality=9)
+    save_video(video, "wan14_i2v_720p_dr_su2_2.mp4", fps=15, quality=9)
